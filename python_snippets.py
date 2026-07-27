d1613d65aa941fb672b34debb2bdde7cf5627ddd
AbyssalCoder <aniketsupermails2005@gmail.com>
2026-07-27 14:30:14 +0530

    Practiced dictionary practice

--- python_snippets.py
+++ python_snippets.py
@@ -88,3 +88,18 @@ def factorial_rec(n):
 
 print(factorial_iter(5))  # 120
 ```
+
+## Python Dictionary Practice
+
+```python
+# Word frequency counter
+text = 'the cat sat on the mat the cat'
+freq = {}
+for word in text.split():
+    freq[word] = freq.get(word, 0) + 1
+print(freq)  # {'the': 3, 'cat': 2, 'sat': 1, 'on': 1, 'mat': 1}
+
+# Using collections.Counter
+from collections import Counter
+print(Counter(text.split()))
+```
